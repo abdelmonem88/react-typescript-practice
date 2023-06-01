63d514f13f27e21ec2b86870838f5df869864e6d
abdelmonem mohamed <abdelmonem1888@gmail.com>
2023-06-01 22:52:49 +0300

    add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
@@ -11,9 +12,26 @@ function App() {
         <Route
           path="/"
           element={
-            <AuthGuard>
-              <Home />
-            </AuthGuard>
+            <ErrorBoundary
+              fallback={
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                  }}
+                >
+                  <h1>Something went wrong</h1>
+                  <p>Try refreshing the page</p>
+                </div>
+              }
+            >
+              <AuthGuard>
+                <Home />
+              </AuthGuard>
+            </ErrorBoundary>
           }
         />
         <Route path="/auth" element={<Auth />} />
